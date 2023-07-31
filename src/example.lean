193f374b43576cbfb055bf9ca72d9fb48810cbf4
Mohammad Amjed <78081740+Mohammad-Amjed@users.noreply.github.com>
2023-08-01 02:50:15 +0400

    Update example.lean

--- src/example.lean
+++ src/example.lean
@@ -4,3 +4,10 @@ theorem hello_world (a b c : ℕ) : a + b + c = a + c + b :=
 begin
   rw [add_assoc, add_comm b, ←add_assoc]
 end
+
+theorem cannot_be_proved_by_tauto (P Q : Prop) : (P → Q) → P → (P ∧ Q) :=
+begin
+intro h1,
+intro h2,
+split,
+end
